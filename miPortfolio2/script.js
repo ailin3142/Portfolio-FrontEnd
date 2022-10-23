586f3d882be53e2c585fef8e20fb3f72153f2d7a
ailin3142 <86528799+ailin3142@users.noreply.github.com>
2022-10-22 21:35:04 -0300

    add files

---
+++ miPortfolio2/script.js
@@ -0,0 +1,48 @@
+//menu responsive
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className ===""){
+        x.className = "responsive";
+
+    }else{
+        x.className = "";
+    }
+}
+
+//seleccion de opciones
+function seleccionar(link){
+    var opciones = document.querySelectorAll('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    link.className = "seleccionado";
+
+    //desaparece menu en modo responsive
+    var x = document.getElementById("nav");
+    x.className ="";
+}
+
+//detectar scroll
+
+window.onscroll = function() {
+    efectoHabilidades();
+};
+
+//animacion barra de habilidades
+function efectoHabilidades() {
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300 && distancia_skills <= 900) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("React").classList.add("barra-progreso4");
+    }else{
+        document.getElementById("html").classList.remove("barra-progreso1");
+        document.getElementById("js").classList.remove("barra-progreso2");
+        document.getElementById("bd").classList.remove("barra-progreso3");
+        document.getElementById("React").classList.remove("barra-progreso4");
+    }
+}
